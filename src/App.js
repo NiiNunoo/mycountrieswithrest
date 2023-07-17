1d3b7b8b09c446c091096288184d25298bb3c299
NiiNunoo <109661342+NiiNunoo@users.noreply.github.com>
2023-07-17 02:11:56 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-import Countries from "./components/countries";
-
+import { BrowserRouter as Router, Link, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Filter from "./components/Filter";
+import Countries from "./components/Countries";
 
 function App() {
-  return <div>
-    <Countries />
-  </div>
+  return (
+    <>
+      <Header />
+      <Filter />
+      <Countries />
+    </>
+  );
 }
 
 export default App;
